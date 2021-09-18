27f8ded6942cb84ed348bf1ecb1b837eb58b5e94
neslihanince <neslihanince94@gmail.com>
2021-09-18 05:19:11 -0400

    User Story1 AC2

--- src/test/java/JiraProject/userStory1/StudentLogin.java
+++ src/test/java/JiraProject/userStory1/StudentLogin.java
@@ -62,5 +62,5 @@ Given student is on the loginPage
 Then verify that the URL is           “https://library2.cybertekschool.com/login.html”
 When student enters valid email address and password
 And student click sign in button
-Then verify that there are 2 models on the page
+Then verify that there are 2 models on the page...
  */
